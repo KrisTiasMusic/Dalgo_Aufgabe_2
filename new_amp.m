d11ae9bc048abebe5fe01795d616156fe9498730
KrisTiasMusic <christoph.eike@student.jade-hs.de>
2015-06-15 16:07:03 +0200

    Limitierung von amp und freq Werten

--- new_amp.m
+++ new_amp.m
@@ -6,6 +6,13 @@ idx = (idx(2) - 517) / (-50) + 1;
 
 amp(idx) = str2double(get(gco,'string'));
 
+if amp(idx) > 1
+    amp(idx) = 1;
+elseif amp(idx) < -1
+    amp(idx) = -1
+end
+
+    
 
 set(t,'TimerFcn',@(~,~)(plot_wave(X,Y,xclick,yclick,amp,freq,phi,xwave,ywave)));
 
